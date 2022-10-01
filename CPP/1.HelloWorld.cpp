777ca7fa1fc508b32083fbee5844d70598cf8950
NishantxST <frey0xd@gmail.com>
2022-10-01 22:30:55 +0530

    C++

---
+++ CPP/1.HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	cout << "This " << "is " << "awesome\n";
+	cout << "Starting c++ " << "Yeah ! ";
+	return 0;
+}
