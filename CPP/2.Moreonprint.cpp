953991bdca122e3d5bdfe5733a3974f21cfbb7a7
NishantxST <frey0xd@gmail.com>
2022-10-02 22:02:28 +0530

    Pro in printing

---
+++ CPP/2.Moreonprint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Boy i love lamborgani\n";
+	cout << "Boy i love lamborgani" << endl ;
+	cout << "\n\n";
+	cout << 2 + 2 + 5<< endl;
+
+	return 0;
+}
