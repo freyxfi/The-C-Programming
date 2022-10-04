aa5b12bbe1d1ef7d407bc82b43188e9236a62252
NishantxST <frey0xd@gmail.com>
2022-10-04 20:30:34 +0530

    Variable

---
+++ CPP/3.Variables.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int x = 7;
+
+	int y = 5;
+	
+
+	// simple calculator
+	
+	int a = 4;
+	int b = 6;
+
+	int sum = a+b ;
+	
+	int sub = a-b;
+
+	int mult = a*b;
+
+	int div = a/b;
+
+	cout << sum <<endl;
+	cout << sub <<endl;
+	cout << mult <<endl;
+	cout << div <<endl;
+	cout << "\n";
+	cout << x <<endl ;
+	cout << y <<endl ;
+
+	return 0;
+
+}
---
+++ CPP/4.cal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a; // addition 
+	int b;
+	/********************/
+	int s; // sub
+	/*******************/
+	int d; // div
+	/*******************/
+	int m; // multi
+	/*******************/
+	int log;
+	/*******************/
+	int Module;
+	/*******************/
+
+	// trigno 
+	int sin;
+	int cos;
+	/*******************/
+	
+	int power;
+	/*******************/
+	int squareroot;
+	/*******************/
+	int root;
+	/*******************/
+	int inverse;
+	/*******************/
+	cout << "Welcome to the NxST Cal";
+
+	return 0;
+}
+	
