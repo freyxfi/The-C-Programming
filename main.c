2b49e58befa2b165560b468daa7427e3be0906e4
Frey <Frey0xd@gmail.com>
2023-07-07 11:42:24 -0400

    Radius of circle

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-  int radius;
+  float radius;
   printf("What is the radius of circle:-");
-  scanf("%i",&radius);
-  double area = 3.1419 * (radius * radius);
+  scanf("%f",&radius);
+  double area = 3.14 * (radius * radius);
   printf("The Area of Circle is %f\n",area);
   return 0;
 }
