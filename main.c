68307b7917c1e514580885f96b17d576907f201b
Frey <Frey0xd@gmail.com>
2023-07-10 13:27:32 -0400

    area of rectangle

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
-  char name[20];
-  printf("whach ya name homie ?:- ");
-  scanf("%s", name);
-  printf("Hello %s\n", name);
-  printf("%s you look lonely i can fix that :)",name);
+	int length, breath, area;
+	printf("What is the leng- ");
+	scanf("%d",&length);
+	printf("What is the breath- ");
+	scanf("%d",&breath);
+	area = (length) * breath;
+	printf("Area of ractangle is %d",area);
   return 0;
 }
