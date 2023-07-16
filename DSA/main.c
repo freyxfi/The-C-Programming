605daf29b3f830d451e8082bab91cff0e988f7e8
Frey <Frey0xd@gmail.com>
2023-07-16 03:57:38 -0400

    Arrays

--- DSA/main.c
+++ DSA/main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-struct node{
-       int data;
-	struct node *link;
-};
-
 int main(){
-	struct node *head = NULL; 
-	head = (struct node *)malloc(sizeof(struct node));
+	
+	int my_number[10];
+	my_number[0] = 11;
+	my_number[1] = 12;
+	my_number[2] = 13;
+	my_number[3] = 14;
+	my_number[4] = 15;
+	my_number[5] = 16;
+	my_number[6] = 17;
+	my_number[7] = 18;
+	my_number[8] = 19;
+	my_number[9] = 20;
 
-	head->data = 45;
-	head->link = NULL;
+	printf("%d\n", my_number[8]);
+	printf("%d\n", my_number[7]);
+	printf("%d\n", my_number[6]);
 
-	printf("Value in the node =>%d\n",head->data);
+	my_number[7] = 42;
+
+	printf("The Number at the index value of 7 is -->%d\n", my_number[7]);
+
+	int i;
+
+	for (i=0; i < 4; i++){
+		printf("%d", my_number[i]);
+	}
 
 	return 0;
 }
