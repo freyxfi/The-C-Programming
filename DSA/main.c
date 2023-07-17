2efb3546298d446c80b736f7071c84f22df93963
Frey <Frey0xd@gmail.com>
2023-07-17 00:35:52 -0400

    stack

--- DSA/main.c
+++ DSA/main.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define MAX 4
+
+int stack_arr[MAX];
+int top = -1; 
+
+void push(int data)
+{
+	if(top == MAX - 1)
+	{
+		printf("Stack OverFlow\n");
+		return;
+	}
+	top = top + 1;
+	stack_arr[top] = data;
+}
+
 int main(){
 	
-	int my_number[10];
-	my_number[0] = 11;
-	my_number[1] = 12;
-	my_number[2] = 13;
-	my_number[3] = 14;
-	my_number[4] = 15;
-	my_number[5] = 16;
-	my_number[6] = 17;
-	my_number[7] = 18;
-	my_number[8] = 19;
-	my_number[9] = 20;
-
-	printf("%d\n", my_number[8]);
-	printf("%d\n", my_number[7]);
-	printf("%d\n", my_number[6]);
-
-	my_number[7] = 42;
-
-	printf("The Number at the index value of 7 is -->%d\n", my_number[7]);
-
-	int i;
-
-	for (i=0; i < 4; i++){
-		printf("%d", my_number[i]);
-	}
+	push(1);
+	push(2);
+	push(3);
+	push(4);
+	push(5);
 
 	return 0;
 }
