9421b9469c8388694e30d74c1b6f8ab4d7bd0746
Frey <Frey0xd@gmail.com>
2023-07-17 22:39:09 -0400

    fib

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 int main(){
-	int length;
-	int breath;
-	int area;
-	printf("What is the leng- ");
-	scanf("%d",&length);
-	printf("What is the breath- ");
-	scanf("%d",&breath);
-	area = (length) * breath;
-	printf("Area of ractangle is %d\n",area);
-  return 0;
+	int a;
+	int b;
+	int result, n, i;
+	printf("Enter The Number of Terms:- ");
+	scanf("%d",&n);
+
+	a = 0;
+	b = 1;
+
+	for(i=1; i<=n; i++)
+	{
+		printf("%d ", a);
+		result = a + b;
+		a = b;
+		b = result;
+	}
+
+	return 0;
 }
